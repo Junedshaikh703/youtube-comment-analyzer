284e2c2c0087216ac1392734a2ee9c822b6788fd
Juned <junedshaikh132006@gmail.com>
2026-03-31 15:26:11 +0530

    fix import path for CI

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,16 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from fastapi.testclient import TestClient
-from app.api import app   # adjust path if needed
+from app.api import app
 
 client = TestClient(app)
-
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-
 
 def test_predict():
     response = client.post("/predict", params={"video_url": "https://youtube.com/test"})
